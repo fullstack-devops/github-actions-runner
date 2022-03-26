b4a6d2177d180d49dc267719e3616db0a95aec4c
eksrha <58111764+eksrha@users.noreply.github.com>
2022-03-26 15:21:18 +0100

    add helper-scripts

---
+++ helper-scripts/gh-entrypoint.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# connection details
+last_char="${GH_URL: -1}"
+[[ $last_char == "/" ]] && GH_URL="${GH_URL::-1}"
+readonly _GH_URL="${GH_URL:-https://github.com}"
+
+last_char="${GH_API_ENDPOINT: -1}"
+[[ $last_char == "/" ]] && GH_API_ENDPOINT="${GH_API_ENDPOINT::-1}"
+readonly _GH_API_ENDPOINT="${GH_API_ENDPOINT:-https://api.github.com}"
+
+# Org/ Repo details
+if [ -z "$GH_ORG" ]; then
+    echo "Please provide Organisation detail by setting GH_ORG"
+    exit 255
+fi
+if [ -z "$GH_REPO" ]; then
+    readonly RUNNER_URL="${_GH_URL}/${GH_ORG}"
+    readonly RUNNER_REG_TOKEN_URL="${_GH_API_ENDPOINT}/orgs/${GH_ORG}/actions/runners/registration-token"
+else
+    readonly RUNNER_URL="${_GH_URL}/${GH_ORG}/${GH_REPO}"
+    readonly RUNNER_REG_TOKEN_URL="${_GH_API_ENDPOINT}/repos/${GH_ORG}/${GH_REPO}/actions/runners/registration-token"
+fi
+
+# access details
+if [ ! -z "$RUNNER_TOKEN" ]; then
+    readonly REG_TOKEN=$RUNNER_TOKEN
+    unset RUNNER_TOKEN
+elif [ ! -z $GH_ACCESS_TOKEN ]; then
+    readonly REG_TOKEN=$(curl -s -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GH_ACCESS_TOKEN" $RUNNER_REG_TOKEN_URL | jq .token --raw-output)
+    unset GH_ACCESS_TOKEN
+else
+    echo "Please provide one of the Environment Variables:"
+    echo "GH_ACCESS_TOKEN, RUNNER_TOKEN"
+    exit 255
+fi
+
+if [ -z ${RUNNER_HOME} ]; then
+    echo "Environment variable 'RUNNER_HOME' is not set"
+    exit 1
+fi
+
+if [ "$KANIKO_ENABLED" == "1" ]; then
+    readonly GH_WORKDIR=$GH_KANIKO_WORKDIR
+    echo "Build container via Kaniko: enabled"
+else
+    readonly GH_WORKDIR=$GH_RUNNER_WORKDIR
+    echo "Build container via Kaniko: disabled"
+fi
+
+echo "Connecting runner to:       $RUNNER_URL"
+echo "Individual Runner Name:     $HOSTNAME"
+echo "Runner Home:                $RUNNER_HOME"
+
+${RUNNER_HOME}/config.sh \
+    --name $HOSTNAME \
+    --token $REG_TOKEN \
+    --work $GH_WORKDIR \
+    --url "$RUNNER_URL" \
+    --labels $GH_RUNNER_LABELS \
+    --unattended \
+    --replace
+echo "Runner configured"
+
+cleanup() {
+    echo "Removing runner..."
+    ${RUNNER_HOME}/config.sh remove --token ${REG_TOKEN}
+    exit 1
+}
+
+trap cleanup 0
+${RUNNER_HOME}/run.sh $RUNNER_ARGS &
+
+echo $! >/tmp/runner_pid
+wait $!
---
+++ helper-scripts/kaniko-wrapper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+logpipe=/kaniko/workspace/log
+returnpipe=/kaniko/workspace/returncode
+if [ -p $logpipe ]; then
+    cat 0<> "$logpipe" <"$logpipe"
+    rm $logpipe
+    echo "Warning: removed stale communication file with kaniko"
+fi
+if [ -p $returnpipe ]; then
+    cat 0<> "$returnpipe" <"$returnpipe"
+    rm $returnpipe
+    echo "Warning: removed stale communication returncode file with kaniko"
+fi
+echo "cd ${PWD}" > /kaniko/workspace/start.sh
+echo "/kaniko/executor --cleanup $@" >> /kaniko/workspace/start.sh
+mkfifo $returnpipe
+mkfifo $logpipe
+cat $logpipe
+rm $logpipe
+returncode=`cat $returnpipe | tr -d "\n"`
+rm $returnpipe
+echo ${returncode}
+exit ${returncode}
